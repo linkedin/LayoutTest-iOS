032b2f5f8800a3c1260a4ab4af793b5d787ef5da
Liam Douglas <liam.douglas@fanduel.com>
2016-02-04 10:44:03 +0000

    Adding new class to add recording of failing snapshots to and test class to make sure everything is under test. Issue 1 - Adding Snapshots for failing tests

---
+++ LayoutTest/TestCase/LYTLayoutFailingTestSnapshotRecorder.h
@@ -0,0 +1,13 @@
+//
+//  LYTLayoutFailingTestSnapshotRecorder.h
+//  LayoutTest
+//
+//  Created by Liam Douglas on 04/02/2016.
+//  Copyright © 2016 LinkedIn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LYTLayoutFailingTestSnapshotRecorder : NSObject
+
+@end
---
+++ LayoutTest/TestCase/LYTLayoutFailingTestSnapshotRecorder.m
@@ -0,0 +1,13 @@
+//
+//  LYTLayoutFailingTestSnapshotRecorder.m
+//  LayoutTest
+//
+//  Created by Liam Douglas on 04/02/2016.
+//  Copyright © 2016 LinkedIn. All rights reserved.
+//
+
+#import "LYTLayoutFailingTestSnapshotRecorder.h"
+
+@implementation LYTLayoutFailingTestSnapshotRecorder
+
+@end

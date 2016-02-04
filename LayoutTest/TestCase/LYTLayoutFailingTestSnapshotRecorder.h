a3617a60fd2adc181a3e1c3a9a9c6b7655a03792
Liam Douglas <liam.douglas@fanduel.com>
2016-02-04 11:01:33 +0000

    Start of moving functionality over and getting it under test. Start with adding viewUnderTest so it can be set on the recorder when needed Issue 1 - Adding Snapshots for failing tests

--- LayoutTest/TestCase/LYTLayoutFailingTestSnapshotRecorder.h
+++ LayoutTest/TestCase/LYTLayoutFailingTestSnapshotRecorder.h
@@ -7,7 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface LYTLayoutFailingTestSnapshotRecorder : NSObject
+
+@property (nonatomic, strong) UIView *viewUnderTest;
 
 @end
--- LayoutTestTests/LayoutTestCaseTests/LYTLayoutFailingTestSnapshotRecorderTests.m
+++ LayoutTestTests/LayoutTestCaseTests/LYTLayoutFailingTestSnapshotRecorderTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "LYTLayoutFailingTestSnapshotRecorder.h"
 
 @interface LYTLayoutFailingTestSnapshotRecorderTests : XCTestCase
 
@@ -14,26 +15,9 @@
 
 @implementation LYTLayoutFailingTestSnapshotRecorderTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+-(void)testCanSetViewUnderTest {
+    LYTLayoutFailingTestSnapshotRecorder *recorder = [[LYTLayoutFailingTestSnapshotRecorder alloc] init];
+    XCTAssertTrue([recorder respondsToSelector:@selector(setViewUnderTest:)]);
 }
 
 @end

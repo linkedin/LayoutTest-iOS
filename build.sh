3c42f8a6ae855a3e570bfccef5b799097b7a4d5e
Doug Young <dyoung@linkedin.com>
2018-01-10 14:44:17 -0800

    Change simulator destinations

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-instruments -s devices
-
 set -o pipefail &&
 time xcodebuild clean test \
     -project LayoutTest.xcodeproj \
     -scheme LayoutTest \
-    -sdk iphonesimulator11.2 \
-    -destination 'platform=iOS Simulator,name=iPhone 6,OS=11.2' \
-    -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.2' \
+    -sdk iphonesimulator10.0 \
+    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' \
+    -destination 'platform=iOS Simulator,name=iPhone 8,OS=11.0' \
 | xcpretty
 
 # Disabling tests on 8.4 for now, because they seem to fail because of simulator problems

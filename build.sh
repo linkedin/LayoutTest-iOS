ba0a12cb3bd08f9de352273e74c1ca665878bf3c
Doug Young <dyoung@linkedin.com>
2018-01-10 11:53:08 -0800

    reverting build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -o pipefail &&
 time xcodebuild clean test \
     -project LayoutTest.xcodeproj \
     -scheme LayoutTest \
-    -sdk iphonesimulator11.2 \
-    -destination 'platform=iOS Simulator,name=iPhone 6,OS=11.2' \
-    -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.2' \
+    -sdk iphonesimulator10 \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.0' \
+    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' \
 | xcpretty
 
 # Disabling tests on 8.4 for now, because they seem to fail because of simulator problems

d9206d4699d29b46c339afbb205efb02686a482e
Doug Young <dyoung@linkedin.com>
2018-01-10 17:04:46 -0800

    switch back to iphone 6 and 7

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ time xcodebuild clean test \
     -scheme LayoutTest \
     -sdk iphonesimulator11.0 \
     -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' \
-    -destination 'platform=iOS Simulator,name=iPhone 8,OS=11.0' \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=11.0' \
 | xcpretty
 
 # Disabling tests on 8.4 for now, because they seem to fail because of simulator problems
